af69a6534a216a0b172d72d2cfad8a4d368f56ca
abel mesfin <abelmesfin555@gmail.com>
2023-04-06 14:53:44 +0300

    output error fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -6,7 +6,8 @@
 --     Column names must be: band_name and lifespan (in years)
 --     You should use attributes formed and split for computing the lifespan
 --     Your script can be executed on any database
-SELECT band_name, (split - formed) AS lifespan
+SELECT band_name,
+    CASE WHEN split IS NOT NULL THEN (split - formed) ELSE (YEAR(CURRENT_DATE()) - formed) END AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
