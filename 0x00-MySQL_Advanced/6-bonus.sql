8e84085cf9c63c0b62ca7df536ab50160e1ba225
abel mesfin <abelmesfin555@gmail.com>
2023-04-06 22:04:05 +0300

    output error fix

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -8,5 +8,8 @@
 
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+    DECLARE project_id INT;
+
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END
