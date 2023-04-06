a67d040421443ddbbcc8c28ab035a9a49de840ce
abel mesfin <abelmesfin555@gmail.com>
2023-04-06 22:39:35 +0300

    output error fix

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,11 +7,11 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    UPDATE users SET average_score = (
+    UPDATE users SET users.average_score = (
         SELECT AVG(score)
         FROM corrections
-        WHERE user_id = user_id
+        WHERE corrections.user_id = user_id
     )
-    WHERE id = user_id;
+    WHERE users.id = user_id;
 END $$
 DELIMITER ;
