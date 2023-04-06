dc2787ea6c174a45bfcd1472b49fada051bbd8e4
abel mesfin <abelmesfin555@gmail.com>
2023-04-06 13:58:09 +0300

    output error fix

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -8,7 +8,7 @@
 --    Your script can be executed on any database
 
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
