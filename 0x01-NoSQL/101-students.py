502b3e1ace79f37d72d2dbfd51db1ae6fe4b5376
abel mesfin <abelmesfin555@gmail.com>
2023-04-11 13:09:48 +0300

    output error fix

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -6,5 +6,15 @@ from pymongo import MongoClient
 
 def top_students(mongo_collection):
     """returns all students soarted by average score"""
-    result = mongo_collection.find().sort('averageScore', pymongo.DESCENDING)
+    students = mongo_collection.find()
+    for student in students:
+        total_score = 0
+        num_topics = len(student["topics"])
+        for topic in student["topics"]:
+            total_score += topic["score"]
+        averageScore = total_score / num_topics
+        mongo_collection.update_one({"_id": student["_id"]},
+                                    {"$set": {"averageScore": averageScore}})
+
+    result = mongo_collection.find().sort('averageScore', -1)
     return result
