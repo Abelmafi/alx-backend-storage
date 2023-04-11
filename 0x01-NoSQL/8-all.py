eb98218f7b1b6197493e852935a5cbda26d4cb30
abel mesfin <abelmesfin555@gmail.com>
2023-04-11 10:46:10 +0300

    rewuirement error fix

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""..."""
+"""a Python function that lists all documents in a collection:"""
 from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
+    """a Python function that lists all documents in a collection:"""
     cursor = mongo_collection.find({})
     doc_list = []
     for item in cursor:
