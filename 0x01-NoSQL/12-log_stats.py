a66bf1d200f3a70d3e4e69086d39a4b8acb5c0fa
abel mesfin <abelmesfin555@gmail.com>
2023-04-11 11:47:15 +0300

    inital commit

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Log stats """
+from pymongo import MongoClient
+
+
+if __name__ == '__main__':
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    ngnix_collection = client.logs.ngnix
+    
+    total_logs = ngnix_collection.count_documents({})
+    print("{} logs".format(total_logs))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = ngnix_collection.count_documents({"method": method})
+        print(f"\t{count} {method} requests")
+    count = ngnix_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{count} status check")
