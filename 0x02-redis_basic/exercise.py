364c3bd9594c05ea093850ce197d1bd9f365ed02
abel mesfin <abelmesfin555@gmail.com>
2023-04-16 22:44:34 +0300

    requirement error fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,17 @@
 import redis
 import uuid
 from typing import Union, Callable, Optional
+import functools
+
+
+def count_calls(func):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        key = func.__qualname__
+        self._redis.incr(key)
+        return func(self, *args, **kwargs)
+    return wrapper
+
 
 class Cache:
     """Cache class"""
@@ -35,3 +46,9 @@ class Cache:
     def get_int(self, key: str) -> Union[int, None]:
         """Get integer data from Redis by key"""
         return self.get(key, fn=lambda x: int(x.decode()))
+
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = uuid.uuid4().hex
+        self._redis.set(key, data)
+        return key
