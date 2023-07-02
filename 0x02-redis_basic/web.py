96ebbe8a457c02bca9155f455ddd88c57ff398de
abel mesfin <abelmesfin555@gmail.com>
2023-07-02 23:37:05 +0300

    commited

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""..."""
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
 from functools import wraps
@@ -11,25 +12,6 @@ redis_store = redis.Redis()
 '''
 
 
-def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    count_key = f'count:{url}'
-    result_key = f'result:{url}'
-
-    redis_store.incr(count_key)
-    result = redis_store.get(result_key)
-
-    if result:
-        return result.decode('utf-8')
-    else:
-        response = requests.get(url)
-        redis_store.setex(result_key, 10, response.text)
-
-        return response.text
-
-
 def data_cacher(method: Callable) -> Callable:
     '''Caches the output of fetched data.
     '''
@@ -37,30 +19,19 @@ def data_cacher(method: Callable) -> Callable:
     def invoker(url) -> str:
         '''The wrapper function for caching the output.
         '''
+        redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
+        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
 
 
-def url_tracker(method: Callable) -> Callable:
-    '''Tracks how many times a URL was accessed.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for tracking the URL.
-        '''
-        redis_store.incr(f'count:{url}')
-        return method(url)
-    return invoker
-
-
-@url_tracker
 @data_cacher
-def get_page_decorated(url: str) -> str:
+def get_page(url: str) -> str:
     '''Returns the content of a URL after caching the request's response,
     and tracking the request.
     '''
